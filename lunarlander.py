35a4bcbc27448f71a92955335f9856e3991127a7
Niccolo Bluhm <niccolobluhm@gmail.com>
2018-04-24 20:52:50 -0700

    Overhauled Mutation + Crossover
    Revamped Mutation and Crossover! --Crossover:
     -Fixed Bug.
     -Changed it so only 1 weight is crossed over between the two ships.
     -1 bias weight is changed with 50% probability
    
    Mutation:
    -Changed it so only 1 weight is mutated, and 1 bias is mutated with probability 50%
    -Instead of replacing the weight with a random number, it is no scaled by a random number between -1 and 3

--- lunarlander.py
+++ lunarlander.py
@@ -211,18 +211,33 @@ class PygView( object ):
         allWeights = np.concatenate((weights1,weights2))
 
         #Mutate anywhere from 5% to %20
-        num_m_weights = int((np.random.rand()*0.10+0.05)*len(allWeights))
-        num_m_intercepts = int((np.random.rand()*0.10+0.05)*len(intercepts))
+        num_m_weights = 1;#int((np.random.rand()*0.10+0.05)*len(allWeights))
+        num_m_intercepts = int(np.round(np.random.rand()));# int((np.random.rand()*0.10+0.05)*len(intercepts))
 
         #Array of indices to mutate
         m_inds_w = np.random.choice(range(0,len(allWeights)), size = num_m_weights, replace = False)
         m_inds_i = np.random.choice(range(0,len(intercepts)), size = num_m_intercepts, replace = False)
 
+
+        mutateFactor = 1 + ((np.random.rand() - 0.5) * 3 + (np.random.rand() - 0.5))
+        for ii in range(len(m_inds_w)):
+            allWeights[m_inds_w[ii]] = allWeights[m_inds_w[ii]] * mutateFactor
+
+        mutateFactor = 1 + ((np.random.rand() - 0.5) * 3 + (np.random.rand() - 0.5))
+        if(num_m_intercepts!=0):
+            for ii in range(len(m_inds_i)):
+                intercepts[m_inds_i[ii]] = allWeights[m_inds_w[ii]] * mutateFactor
+
+        #Old Implementation
+        """
         for ii in range(len(m_inds_w)):
             allWeights[m_inds_w[ii]] = np.random.rand()*2-1
 
-        for ii in range(len(m_inds_i)):
-            intercepts[m_inds_i[ii]] = np.random.rand()*2-1
+        if(num_m_intercepts!=0):
+            for ii in range(len(m_inds_i)):
+                intercepts[m_inds_i[ii]] = np.random.rand()*2-1
+        """
+
 
         #Reconstruct
         intercepts_0 = intercepts[range(s0)]
@@ -254,17 +269,17 @@ class PygView( object ):
         intercepts= np.concatenate( (NN.intercepts_[0],NN.intercepts_[1]))
         weights1 = NN.coefs_[0].flatten()
         weights2 = NN.coefs_[1].flatten()
-        allWeights = np.concatenate((intercepts,weights1,weights2))
+        allWeights = np.concatenate((weights1,weights2))
 
         intercepts2= np.concatenate( (NN2.intercepts_[0],NN2.intercepts_[1]))
         weights12 = NN2.coefs_[0].flatten()
         weights22 = NN2.coefs_[1].flatten()
-        allWeights2 = np.concatenate((intercepts2,weights12,weights22))
+        allWeights2 = np.concatenate((weights12,weights22))
 
         #Crossover anywhere from 20% to %60
         #Number of weights and intercepts to crossover
-        num_m_weights = int((np.random.rand()*0.4+0.2)*len(allWeights))
-        num_m_intercepts = int((np.random.rand()*0.4+0.2)*len(intercepts))
+        num_m_weights = 1 #int( np.ceil( (np.random.rand()*0.1)*len(allWeights)) )
+        num_m_intercepts = int(np.round(np.random.rand())); #np.round((np.random.rand()*0.1)*len(intercepts))
 
         m_inds_w = np.random.choice(range(0,len(allWeights)), size = num_m_weights, replace = False)
         m_inds_i = np.random.choice(range(0,len(intercepts)), size = num_m_intercepts, replace = False)
@@ -272,8 +287,9 @@ class PygView( object ):
         for ii in range(len(m_inds_w)):
             allWeights[m_inds_w[ii]] = allWeights2[m_inds_w[ii]]
 
-        for ii in range(len(m_inds_i)):
-            intercepts[m_inds_i[ii]] = intercepts2[m_inds_i[ii]]
+        if(num_m_intercepts !=0):
+            for ii in range(len(m_inds_i)):
+                intercepts[m_inds_i[ii]] = intercepts2[m_inds_i[ii]]
 
         #Reconstruct
         intercepts_0 = intercepts[range(s0)]
@@ -363,6 +379,7 @@ class PygView( object ):
 
             NN = self.crossover(sortedShips[parents[0]],sortedShips[parents[1]])
             theNewMlp = self.mutate(NN)
+            #theNewMlp = self.mutate(sortedShips[parents[0]])
 
             newShips.append(deepcopy(theNewMlp))
 
